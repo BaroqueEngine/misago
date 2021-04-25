c7be7797d47173b240722491f2fcf9beb0e46b87
BaroqueEngine <BaroqueEngine@users.noreply.github.com>
2021-04-25 23:16:44 +0000

    deploy: 678b0d72a565b6cc01a6949a71d6dde741fe6964

---
+++ static/code/23ba25cc/app.ts
@@ -0,0 +1,44 @@
+import * as p5 from "p5";
+
+new p5((p: p5) => {
+  let image: p5.Image;
+
+  p.preload = () => {
+    image = p.loadImage("./0.jpg");
+  };
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    image.loadPixels();
+
+    for (let y = 0; y < image.height; y++) {
+      for (let x = 0; x < image.width; x++) {
+        const color = getPixel(x, y);
+
+        const r = p.red(color);
+        const g = p.green(color);
+        const b = p.blue(color);
+
+        const gray = 0.299 * r + 0.587 * g + 0.114 * b;
+        const hue = p.floor(p.map(gray, 0, 255, 240, 0));
+        const hsb = p.color(`hsb(${hue}, 100%, 100%)`);
+        setPixel(x, y, [p.red(hsb), p.green(hsb), p.blue(hsb)]);
+      }
+    }
+
+    image.updatePixels();
+    p.image(image, 0, 0);
+  };
+
+  function getPixel(x: number, y: number): number[] {
+    const i = (y * image.width + x) * 4;
+    return [image.pixels[i], image.pixels[i + 1], image.pixels[i + 2], image.pixels[i + 3]];
+  }
+
+  function setPixel(x: number, y: number, color: number[]): void {
+    const i = (y * image.width + x) * 4;
+    image.pixels[i + 0] = color[0];
+    image.pixels[i + 1] = color[1];
+    image.pixels[i + 2] = color[2];
+  }
+});
---
+++ static/code/359993fd/app.ts
@@ -0,0 +1,41 @@
+import * as p5 from "p5";
+
+new p5((p: p5) => {
+  let image: p5.Image;
+
+  p.preload = () => {
+    image = p.loadImage("./0.jpg");
+  };
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+    image.loadPixels();
+
+    for (let y = 0; y < image.height; y++) {
+      for (let x = 0; x < image.width; x++) {
+        const color = getPixel(x, y);
+        const r = p.red(color);
+        const g = p.green(color);
+        const b = p.blue(color);
+        const gray = 0.299 * r + 0.587 * g + 0.114 * b;
+        setPixel(x, y, [gray, gray, gray]);
+      }
+    }
+
+    image.updatePixels();
+    p.image(image, 0, 0);
+  };
+
+  function getPixel(x: number, y: number): number[] {
+    const i = (y * image.width + x) * 4;
+    return [image.pixels[i], image.pixels[i + 1], image.pixels[i + 2], image.pixels[i + 3]];
+  }
+
+  function setPixel(x: number, y: number, color: number[]): void {
+    const i = (y * image.width + x) * 4;
+    image.pixels[i + 0] = color[0];
+    image.pixels[i + 1] = color[1];
+    image.pixels[i + 2] = color[2];
+  }
+});
