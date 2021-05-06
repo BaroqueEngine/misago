5c14507192c5c67dd5f9329c51fff7deea7e06c7
Baroque Engine <rsakane+github@gmail.com>
2021-05-06 13:53:58 +0900

    記事追加: マップ生成2

---
+++ static/code/92c5bea7/app.ts
@@ -0,0 +1,74 @@
+import * as p5 from "p5";
+
+const CellType = {
+  sea: "sea",
+  land: "land",
+};
+type CellType = typeof CellType[keyof typeof CellType];
+
+new p5((p: p5) => {
+  const tileWidth = 128;
+  const tileHeight = 64;
+  const iterations = 40000;
+  const tileSize = 5;
+  const fillProbability = 0.4;
+  let map: CellType[][];
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+    initMap();
+
+    for (let i = 0; i < iterations; i++) {
+      generateMap();
+    }
+
+    p.strokeWeight(1);
+    for (let y = 0; y < tileHeight; y++) {
+      for (let x = 0; x < tileWidth; x++) {
+        if (map[y][x] === CellType.sea) {
+          p.fill("#4977bc");
+          p.rect(x * tileSize, y * tileSize, tileSize, tileSize);
+        }
+      }
+    }
+  };
+
+  function initMap(): void {
+    map = [];
+
+    for (let y = 0; y < tileHeight; y++) {
+      map[y] = [];
+      for (let x = 0; x < tileWidth; x++) {
+        map[y][x] = p.random(1.0) < fillProbability ? CellType.land : CellType.sea;
+      }
+    }
+  }
+
+  function generateMap(): void {
+    const tx = p.floor(p.random(tileWidth));
+    const ty = p.floor(p.random(tileHeight));
+
+    map[ty][tx] = 4 < getNeighborSeaCount(tx, ty) ? CellType.sea : CellType.land;
+  }
+
+  function getNeighborSeaCount(x: number, y: number): number {
+    let count = 0;
+    for (let ty = y - 1; ty <= y + 1; ty++) {
+      for (let tx = x - 1; tx <= x + 1; tx++) {
+        if (isSea(tx, ty)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  function onBoard(x: number, y: number): boolean {
+    return 0 <= x && x < tileWidth && 0 <= y && y < tileHeight;
+  }
+
+  function isSea(x: number, y: number): boolean {
+    return onBoard(x, y) && map[y][x] === CellType.sea;
+  }
+});
