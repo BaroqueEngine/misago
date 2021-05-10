ff270c3f3a8a281b41f2e9fbe3ba498f10ddce4f
Baroque Engine <rsakane+github@gmail.com>
2021-05-10 23:21:34 +0900

    記事の追加: パーティクルに重力を適用する

---
+++ static/code/45784b8b/app.ts
@@ -0,0 +1,55 @@
+import * as p5 from "p5";
+import { Particle, init, update, isDead } from "./particle";
+
+new p5((p: p5) => {
+  const color = {
+    background: "#02121e",
+    fill: 0xf0f0f0,
+  };
+  const interval = 10;
+  const maxParticle = 100;
+  const gravity = 0.1;
+  let particles: Particle[];
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+    p.textAlign(p.CENTER);
+    particles = [];
+  };
+
+  p.draw = () => {
+    p.background(color.background);
+    p.noStroke();
+
+    p.fill("#fff");
+    p.text(`${particles.length} / ${maxParticle}`, p.width / 2, p.height / 2);
+
+    if (p.frameCount % interval === 0 && particles.length < maxParticle) {
+      addParticle();
+    }
+
+    for (let i = 0; i < particles.length; i++) {
+      const particle = particles[i];
+      update(particle, gravity);
+      p.fill(color.fill, particle.lifespan);
+      p.circle(particle.x, particle.y, particle.radius);
+
+      if (isDead(particle)) {
+        particles.splice(i, 1);
+        i--;
+      }
+    }
+  };
+
+  function addParticle(): void {
+    const vx = p.random(-3, 3);
+    const vy = p.random(-10, -15);
+    const radius = p.random(5, 60);
+    const lifespan = 255;
+    const damage = p.random(1, 1);
+    const opacity = p.random(40, 255);
+    const particle = init(p.windowWidth / 2, p.windowHeight, vx, vy, radius, opacity, lifespan, damage);
+    particles.push(particle);
+  }
+});
---
+++ static/play/45784b8b/main.js
@@ -0,0 +1,54 @@
+import { init, update, isDead } from "./particle.js";
+new p5((p) => {
+  const color = {
+    background: "#02121e",
+    fill: 0xf0f0f0,
+  };
+  const interval = 10;
+  const maxParticle = 100;
+  const gravity = 0.1;
+  let particles;
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textAlign(p.CENTER);
+    particles = [];
+  };
+  p.draw = () => {
+    p.background(color.background);
+    p.noStroke();
+    p.fill("#fff");
+    p.text(`${particles.length} / ${maxParticle}`, p.width / 2, p.height / 2);
+    if (p.frameCount % interval === 0 && particles.length < maxParticle) {
+      addParticle();
+    }
+    for (let i = 0; i < particles.length; i++) {
+      const particle = particles[i];
+      update(particle, gravity);
+      p.fill(color.fill, particle.lifespan);
+      p.circle(particle.x, particle.y, particle.radius);
+      if (isDead(particle)) {
+        particles.splice(i, 1);
+        i--;
+      }
+    }
+  };
+  function addParticle() {
+    const vx = p.random(-3, 3);
+    const vy = p.random(-10, -15);
+    const radius = p.random(5, 60);
+    const lifespan = 255;
+    const damage = p.random(1, 1);
+    const opacity = p.random(40, 255);
+    const particle = init(
+      p.windowWidth / 2,
+      p.windowHeight,
+      vx,
+      vy,
+      radius,
+      opacity,
+      lifespan,
+      damage
+    );
+    particles.push(particle);
+  }
+});
