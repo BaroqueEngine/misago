5b0b80c14146f618aaa9d096b8e7e7e84ef8996f
BaroqueEngine <BaroqueEngine@users.noreply.github.com>
2021-06-24 10:19:14 +0000

    deploy: 1ccc5268977cce06bd1b5c22b3e6dcad260d237f

--- static/code/946c9cf0/point.ts
+++ static/code/946c9cf0/point.ts
@@ -7,7 +7,7 @@ export function init(x: number, y: number): Point {
   return { x, y };
 }
 
-export function leap(a: Point, b: Point, t: number): Point {
+export function lerp(a: Point, b: Point, t: number): Point {
   const x = a.x * (1 - t) + b.x * t;
   const y = a.y * (1 - t) + b.y * t;
 
